a66505747e937f9ccb0e0f0d032ecd82d8bf9c63
syedrawoof <syedrawoof115@gmail.com>
2024-04-03 14:34:11 +0530

    understand the react flow and structure

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div>Chai from Component</div>
+  )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <>
+      <h1>Learning React JS with vite</h1>
+      <Chai />
+      <p>Test Para</p>
+    </>
+  );
+}
+
+export default App;
