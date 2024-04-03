bcd1a7eac116ae3c57023b3ae4b331637c443c00
syedrawoof <syedrawoof115@gmail.com>
2024-04-03 14:56:35 +0530

    create a custom react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,10 @@
 import Chai from "./Chai";
 function App() {
+  const username="Syed Rawoof";
   return (
     <>
       <h1>Learning React JS with vite</h1>
+      <h1>Learning React JS by {username}</h1>
       <Chai />
       <p>Test Para</p>
     </>
